8e4e58d4fadf98d9a48c6837d50e1dd6e58066f2
ngocbao091102 <118184330+ngocbao091102@users.noreply.github.com>
2022-12-06 16:59:35 +0700

    Add files via upload

---
+++ LAB8.sql
@@ -0,0 +1,153 @@
+﻿-- LAB8 ( Tiếp theo )
+
+---2---
+
+
+--2 Tạo quan hệ SANPHAM1 chứa toàn bộ dữ liệu của quan hệ SANPHAM. Tạo quan hệ
+--KHACHHANG1 chứa toàn bộ dữ liệu của quan hệ KHACHHANG.
+GO
+SELECT * FROM SANPHAM,KHACHHANG
+GO
+
+--3 Cập nhật giá tăng 5% đối với những sản phẩm do “Thai Lan” sản xuất (cho quan hệ SANPHAM1)
+GO
+UPDATE SANPHAM SET GIA=GIA+GIA/(100/5) WHERE NUOCSX='Thai Lan'
+GO
+
+
+--4 Cập nhật giá giảm 5% đối với những sản phẩm do “Trung Quoc” sản xuất có giá từ 10.000 trở xuống
+--(cho quan hệ SANPHAM1).
+UPDATE SANPHAM SET GIA =GIA-GIA*(100/5) WHERE NUOCSX='TRUNG QUOC' AND GIA <10000
+
+
+--5 Cập nhật giá trị LOAIKH là “Vip” đối với những khách hàng đăng ký thành viên trước ngày
+--1/1/2007 có doanh số từ 10.000.000 trở lên hoặc khách hàng đăng ký thành viên từ 1/1/2007 trở về
+--sau có doanh số từ 2.000.000 trở lên (cho quan hệ KHACHHANG1).
+UPDATE KHACHHANG SET LOAIKH ='Vip' WHERE (NGDK<'2007/1/1' AND DOANHSO>=10000000) OR (NGDK>'2007/1/1' AND DOANHSO >=2000000)
+
+
+---3---
+
+--1.	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE NUOCSX = 'TRUNG QUOC'
+--2.	In ra danh sách các sản phẩm (MASP, TENSP) có đơn vị tính là “cay”, ”quyen”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE DVT IN('CAY', 'QUYEN')
+--3.	In ra danh sách các sản phẩm (MASP,TENSP) có mã sản phẩm bắt đầu là “B” và kết thúc là “01”.
+SELECT MASP, TENSP
+FROM SANPHAM
+WHERE MASP LIKE'B%01'
+
+
+---4---	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quốc” sản xuất có giá từ 30.000 đến 40.000.
+
+SELECT MASP,TENSP,NUOCSX
+FROM SANPHAM
+WHERE NUOCSX = 'TRUNG QUOC'
+AND GIA BETWEEN 30000 AND 40000
+
+
+---5---	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” hoặc “Thai Lan” sản xuất có giá từ 30.000 đến 40.000.
+
+SELECT MASP, TENSP, NUOCSX
+FROM SANPHAM
+WHERE (NUOCSX = 'TRUNG QUOC' OR NUOCSX = 'THAI LAN') AND GIA BETWEEN 30000 AND 40000
+
+
+---6---	In ra các số hóa đơn, trị giá hóa đơn bán ra trong ngày 1/1/2007 và ngày 2/1/2007.
+
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE NGHD >= '1/1/2007' AND NGHD <= '1/2/2007'
+
+
+---7---	In ra các số hóa đơn, trị giá hóa đơn trong tháng 1/2007, sắp xếp theo ngày (tăng dần) và trị giá của hóa đơn (giảm dần).
+
+SELECT SOHD, TRIGIA
+FROM HOADON
+WHERE MONTH(NGHD) = 1 AND YEAR(NGHD) = 2007
+ORDER BY NGHD ASC, TRIGIA DESC
+
+
+---8---	In ra danh sách các khách hàng (MAKH, HOTEN) đã mua hàng trong ngày 1/1/2007.
+
+SELECT K.MAKH, HOTEN
+FROM KHACHHANG K INNER JOIN HOADON H
+ON K.MAKH = H.MAKH
+WHERE NGHD = '1/1/2007'
+
+
+---9---	In ra số hóa đơn, trị giá các hóa đơn do nhân viên có tên “Nguyen Van B” lập trong ngày 28/10/2006.
+
+SELECT SOHD, TRIGIA
+FROM HOADON H INNER JOIN NHANVIEN N
+ON H.MANV = N.MANV
+WHERE NGHD = '10/28/2006'
+AND HOTEN = 'NGUYEN VAN B'
+
+
+---10---	In ra danh sách các sản phẩm (MASP,TENSP) được khách hàng có tên “Nguyen Van A” mua trong tháng 10/2006.
+
+SELECT DISTINCT S.MASP, TENSP
+FROM SANPHAM S INNER JOIN CTHD C
+ON S.MASP = C.MASP
+AND EXISTS(SELECT *
+FROM CTHD C INNER JOIN HOADON H
+ON C.SOHD = H.SOHD
+AND MONTH(NGHD) = 10 AND YEAR(NGHD) = 2006 AND MAKH IN(SELECT H.MAKH
+FROM HOADON H INNER JOIN KHACHHANG K
+ON H.MAKH = K.MAKH
+WHERE HOTEN = 'NGUYEN VAN A') AND S.MASP = C.MASP)
+
+
+---11---	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”.
+
+SELECT SOHD
+FROM CTHD
+WHERE MASP IN ('BB01', 'BB02')
+
+
+---12---	Tìm các số hóa đơn đã mua sản phẩm có mã số “BB01” hoặc “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+
+SELECT SOHD
+FROM CTHD
+WHERE MASP IN ('BB01', 'BB02')
+AND SL BETWEEN 10 AND 20
+
+
+---13---	Tìm các số hóa đơn mua cùng lúc 2 sản phẩm có mã số “BB01” và “BB02”, mỗi sản phẩm mua với số lượng từ 10 đến 20.
+
+SELECT SOHD
+FROM CTHD A
+WHERE A.MASP = 'BB01'
+AND SL BETWEEN 10 AND 20
+AND EXISTS(SELECT *
+FROM CTHD B
+WHERE B.MASP = 'BB02'
+AND SL BETWEEN 10 AND 20
+AND A.SOHD = B.SOHD)
+
+
+---14---	In ra danh sách các sản phẩm (MASP,TENSP) do “Trung Quoc” sản xuất hoặc các sản phẩm được bán ra trong ngày 1/1/2007.
+
+SELECT DISTINCT S.MASP, TENSP
+FROM SANPHAM S INNER JOIN CTHD C
+ON S.MASP = C.MASP
+WHERE NUOCSX = 'TRUNG QUOC'
+AND C.SOHD IN(SELECT DISTINCT C2.SOHD
+FROM CTHD C2 INNER JOIN HOADON H
+ON C2.SOHD = H.SOHD
+WHERE NGHD ='1/1/2007')
+
+
+---15---	In ra danh sách các sản phẩm (MASP,TENSP) không bán được.
+
+SELECT S.MASP, TENSP
+FROM SANPHAM S
+WHERE NOT EXISTS(SELECT * 
+FROM SANPHAM S2 INNER JOIN CTHD C
+ON S2.MASP = C.MASP
+AND S2.MASP = S.MASP)
